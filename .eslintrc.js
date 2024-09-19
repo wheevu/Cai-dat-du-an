a329b15ced5d773d1431650dde688b51dfebad6e
wheevu <101315525+wheevu@users.noreply.github.com>
2024-09-19 09:13:25 +0700

    Cai dat express chay thong bao xin chao

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-env: {
-node: true,
-commonjs: true,
-es2021: true,
-},
-extends: ['eslint:recommended','prettier'],
+    env: {
+        node: true,
+        commonjs: true,
+        es2021: true,
+    },
+    extends: ['eslint:recommended', 'prettier'],
 };
