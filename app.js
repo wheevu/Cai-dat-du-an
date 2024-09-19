6433c19fee714a9e9b781ce4da4db69da6830bf7
wheevu <101315525+wheevu@users.noreply.github.com>
2024-09-19 09:25:27 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const cors = require("cors");
 
 const app = express();
 
-app.use (cors());
-app.use (express.json ());
+const contactsRouter = require("./app/routes/contact.route");
+
+app.use(cors());
+app.use(express.json ());
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
-}) ;
+});
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,28 @@
+exports. create = (req, res) => {
+res.send({ message: "create handler" });
+};
+
+exports.findALL = (req, res) => {
+res.send({ message: "findAll handler" });
+};
+
+exports. findone = (req, res) => {
+res.send({ message: "findOne handler" });
+};
+
+exports.update = (req, res) => {
+res.send({ message: "update handler" });
+};
+
+exports. delete = (req, res) => {
+res.send({ message: "delete handler" });
+};
+
+exports. deLeteALL = (req, res) => {
+res. send({ message: "deleteAll handler" });
+};
+
+exports. findALLFavorite = (req, res) => {
+res. send({ message: "findAllFavorite handler" });
+};
+
