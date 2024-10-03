b9d8eb2fe5ecdf19783de4bf9247e0443a956609
wheevu <101315525+wheevu@users.noreply.github.com>
2024-10-03 09:23:02 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -32,3 +32,4 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
+
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require(" ./app/utils/mongodb.util");
 
 // start server
 
@@ -7,3 +8,20 @@ const PORT = config.app.port;
 app.listen (PORT, () => {
 console.log(`Server is running on port ${PORT}.`);
 });
+
+async function startServer() {
+    try {
+        await MongoDB.connect(config.db.uri);
+        console. Log("Connected to the database!");
+        
+        const PORT = config.app.port;
+        app.listen (PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console. Log("Cannot connect to the database!", error); 
+        process.exit;
+    }
+}
+startServer;
+
